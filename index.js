7708e4513b4e7e18d299bba4d31a97a223a2a6af
adrianosuprine <adrianosuprine@gmail.com>
2023-12-24 06:29:58 +0300

    Looping in an array

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array,name){
+    const newArray = [];
+    for(let m = 0; m < array.length; m++){
+    var message = (`Thank you, ${array[m]}, for the wonderful ${name} gift!`);
+    newArray.push(message);
+    }
+    return newArray;
+
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+function countDown(){
+    let num = 10 ;
+    while (num >= 0){
+        console.log(num--);
+    }
+}
+countDown();
